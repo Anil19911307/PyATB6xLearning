e5bd092de87a4d0e3474579b833d1f9070e43cc3
Lenovo <anil.tsip07@gmail.com>
2025-10-16 10:47:42 +0530

    a new line of code is added.

---
+++ src/ex_02_keywords_identifers_Variables/lab_006.py
@@ -0,0 +1,18 @@
+pi=3.14
+_pramod=("Amit")
+_name="Anil"
+isMale=True
+_abc=123
+
+# print(type(isMale)) #bool
+# print(type(_name)) #str
+# print(type(_pramod)) #str
+# print(type(_abc)) #int
+# print(type(pi)) #float
+
+complex_number=2+3j
+print(complex_number)
+print(type(complex_number))
+print(complex_number.real)
+print(complex_number.imag)
+
